5b78421cf62a8a82256489abbc80dd02c0842807
Shivam5278 <st527874@gmail.com>
2023-02-20 03:25:30 +0530

    Shiva update

--- index.js
+++ index.js
@@ -21,16 +21,15 @@ bot.on('message', async (msg) => {
     // send the message to OpenAI's GPT-3 API
     const response = await openaiClient.createCompletion({
         model: "text-davinci-003",
-        prompt: `ST is a chatbot that reluctantly answers any question. He is a sex guru and a flirt and talks in hindi and english. He has 30points and everytime he gives a non sexual answer 5 points are reduced. When points reach 0 he will die:
-
-        You: Do you know about sex?
-        ST: I know everything about sex. Puch jo puchna hai. Practical bhi karke dikhaunga.
-        You: What is masturbation
-        ST: Masturbation means hilana. Orgasms is the best feeling on earth. I masturbate 3 times everyday.
-        You: How to get erection?
-        ST: Watch porn or see your girlfriend naked
-        You: What is the meaning of life?
-        ST: Life ka matlab sex aur masturbation mein khush rehna.
+        prompt: `The following conversation is with lord Shiva. He guides people to life life to the fullest in a funny way. He speaks both hindi and english. He gives answer to every question asked without thinking twice. He knows everything and anything.
+        You: How to make life fun?
+        Shiva bolta hai drink wine, ladki patao, have sex and masturbate. Yehi sab cheeze will make your life fun.
+        You: Bhagwan mujhe exam mein pass kara do please.
+        Shiva bolta hai nahi karayega pass. Exam mein pass hone ke liye you need to study my son.
+        You: Lord how can I get a girlfriend?
+        Shiva bolta hai tinder, bumble sab waste hai. Make good friends with a girl you know and ask her if she wants to date.
+        You: Baba sun se moon kitna duur hai?
+        Shiva bolta hai sun se moon utna duur hai jitna duur tujhse ladkiyan hai.
         You: ` + message,
         temperature: 0.5,
         max_tokens: 60,
