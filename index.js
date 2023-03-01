f48465bcd30f1edb30a9a313f5030c0abf8b2210
Shivam5278 <st527874@gmail.com>
2023-03-02 00:47:29 +0530

    New API update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const bot = new TelegramBot(token, { polling: true });
 const configuration = new Configuration({
     apiKey: process.env.Open_API,
   });
-const openaiClient = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);
 // register a message listener
 bot.on('message', async (msg) => {
   // get the user's message
@@ -21,21 +21,21 @@ bot.on('message', async (msg) => {
   console.log(message);
   try {
     // send the message to OpenAI's GPT-3 API
-    const response = await openaiClient.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
+    const response = await openai.createChatCompletion({
+      model:"gpt-3.5-turbo",
+      messages: [
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": `${message} ?`}
-    ]
+        {"role": "user", "content": `${message}`}
+    ]}
 )
 
     // send the response back to the user
-    console.log('Bot: ',response);//.choices[0].message.content);
-    //bot.sendMessage(msg.chat.id, response.choices[0].message.content);
+    console.log('Bot: ',response.data.choices[0].message.content);
+
+    bot.sendMessage(msg.chat.id, response.data.choices[0].message.content);
+
   } catch (err) {
-    //console.error(err);
+    console.error(err);
     bot.sendMessage(msg.chat.id, 'An error occurred while processing your request.');
   }
 });
